e8d5cb4d6bf4c7ea552d4ec06f6dc625c503e051
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-07 15:07:55 -0500

    just about finished the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,31 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavoriteNumber = 27;
+        System.out.println(myFavoriteNumber);
         System.out.println("Hello, World");
+
+        final String myString = "Ditto";
+        System.out.println(myString);
+
+        float myNumber = 314;
+        System.out.println(myNumber);
+
+       /* int x = 5;
+        System.out.println(x++);
+        System.out.println(x);*/
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+
+
+
+
     }
 }
