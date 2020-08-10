22ca6da88e691b651ed3347c502963b05f770fd8
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-10 10:07:50 -0500

    new java exercises

---
+++ src/scannerPractice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class scannerPractice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int userInt = 0;
+        while (userInt == 0) {
+            System.out.print("\n\nEnter some integer: ");
+        }
+
+
+
+
+
+
+    }
+
+
+}
