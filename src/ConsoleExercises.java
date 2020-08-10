cdf79e2291ceb138e74f752a455c7040c4773a68
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-10 12:28:11 -0500

    console exercises near complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main (String[] args) {
         double pi = 3.14159;
-        System.out.printf("\n\n ")
+        System.out.printf("\n\n The value of pi is %.2f", pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n Choose your number");
+        String userNumber = scanner.next();
+        System.out.printf("\n\nyour chosen number is: %s" , userNumber);
+
+
+
+        System.out.println("\n Three words, Go!");
+
+
+
     }
-
-
 }
