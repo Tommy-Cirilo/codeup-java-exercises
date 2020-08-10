0c09438ec3d20fe9d5892e9b0a2681b99ada4fe0
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-10 16:48:41 -0500

    finished first do while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,23 @@
 public class ControlFlowExercises {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+//        DO while loop to count by two, till the counter hits 100
+        int i1 = 2;
+        do {
+            System.out.println(i1);
+            i1 += 2;
+        } while (i1 <= 100);
+        i1 = i1 + 2;
+
+
+
+
+
+
 
     }
 }
