a53371bf251804388c33f6829b2bb8ec1370a367
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-11 16:13:24 -0500

    finished 8-11-20 exercises

--- src/Bob.java
+++ src/Bob.java
@@ -1,41 +1,53 @@
 import java.util.Scanner;
 
 public class Bob {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        // Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+        // He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+        // He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+        // He answers 'Whatever.' to anything else.
 
-        Scanner scanner = new Scanner(System.in);
+//        MethodsLec thisMethod = new MethodsLec();
+//        thisMethod.sayHi("Casey");
 
-        boolean again = true;
+        // double number = Math.pow(4,3);
+        // Math mathTool = new Math();
+        // MethodsLec.sayHi("casey");
+
+        Scanner sc = new Scanner(System.in);
+        boolean again = true; // should we continue or not?
         do {
-            System.out.println("Type @ Bob:");
-            String userInput = scanner.nextLine();
+            System.out.print("Type @ Bob: ");
+            String userInput = sc.nextLine(); // either a string, or empty string
 
-            if (userInput.isEmpty() ) {
-                System.out.println("whatever");
+            if (userInput.isEmpty()) {
+                System.out.println("Fine, be that way.");
             } else {
-                char lastchar = userInput.charAt(userInput.length()-1);
-                switch (lastchar) {
+                char lastChar = userInput.charAt(userInput.length() - 1); // last character
+                switch (lastChar) {
                     case '?':
-                        System.out.println("sure");
+                        System.out.println("Sure");
                         break;
                     case '!':
-                        System.out.println("whoa chill out");
+                        System.out.println("Whoa, chill out!");
                         break;
                     default:
-                        System.out.println("whatever");
+                        System.out.println("Whatever");
                         break;
+                }
 
-
-                    System.out.print("Dm Bob again? [y/N: ");
-                    String response = sc.nextLine();
-                    if (!response.trim().equalsIgnoreCase('y')) {
-                        again = false;
-                    }
+                // ask the user if they would like to continue
+                System.out.print("DM Bob again? [y/N]: ");
+                String response = sc.nextLine();
+                if (!response.trim().equalsIgnoreCase("y")) {
+                    again = false;
                 }
             }
 
-        } while(again);
+        } while (again);
 
+        // user has clicked something other than y/Y, so exit with a message
+        System.out.println("kthxbai");
 
     }
 }
