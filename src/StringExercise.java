981b6cf3bc8d5c22bb9f7ae3889b3b1c97d813cb
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-11 13:46:08 -0500

    finished string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class StringExercise {
     public static void main (String[] args) {
+
+    String output = "we don't need no education";
+    output = output + "\nwe don't need no thought control";
+        System.out.println(output);
 
     }
 }
