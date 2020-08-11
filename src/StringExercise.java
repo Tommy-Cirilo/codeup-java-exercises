3604b767fb775de764e0819e7644ee61a2a3121d
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-11 14:09:27 -0500

    completed string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class StringExercise {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-    String output = "we don't need no education";
-    output = output + "\nwe don't need no thought control";
+        String output = "we don't need no education \n we don't need no thought control";
+//    output = output + "\nwe don't need no thought control";
         System.out.println(output);
-
+        output = "in windows, the main drive is usually C:\\";
+        System.out.println(output);
+        output = "i can do backslashes \\, double backslashes \\\\, \n and the amazing triple backslash \\\\\\ !";
     }
 }
