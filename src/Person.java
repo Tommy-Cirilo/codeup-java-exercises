161c56ece404f0ed148f3fde6d8924b74ee1e69c
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-14 11:15:08 -0500

    new test file

--- src/Person.java
+++ src/Person.java
@@ -6,14 +6,33 @@ Create Person class inside of src that has a private name property that is a str
 the person's name to the passed string.*/
 
 public class Person {
-    public Person() {
-        System.out.println("A Person is being created!");
-    }
     public static void main(String[] args) {
 
+        /*Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);*/
+
+        /*Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1 == person2);*/
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
 
     }
+
+    public Person(String name) {
+        System.out.println(name);
+    }
+
     private String name;
+
     /*Create a `main` method on the class that creates a new `Person` object and
     tests the above methods.*/
         public String getName(){
