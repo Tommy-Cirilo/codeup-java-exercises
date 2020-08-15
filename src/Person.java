ce912f7c38ad91389f2591f2e6680c4c839655cd
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-15 10:31:11 -0500

    finished person exercise

--- src/Person.java
+++ src/Person.java
@@ -6,6 +6,31 @@ Create Person class inside of src that has a private name property that is a str
 the person's name to the passed string.*/
 
 public class Person {
+    //this creates a new person with a name
+    public Person(String name) {
+        this.name = name;
+    }
+    //this declares the name as private
+    private String name;
+
+    /*Create a `main` method on the class that creates a new `Person` object and
+    tests the above methods.*/
+    public String getName() {
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name) {
+//TODO: change the name property to the passed value
+        this.name = name;
+    }
+
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+        System.out.println("hello " + name);
+    }
+
+//    the three sets of code here each test the other methods
     public static void main(String[] args) {
 
         /*Person person1 = new Person("John");
@@ -17,39 +42,14 @@ public class Person {
         Person person2 = person1;
         System.out.println(person1 == person2);*/
 
-        Person person1 = new Person("John");
+        /*Person person1 = new Person("John");
         Person person2 = person1;
         System.out.println(person1.getName());
         System.out.println(person2.getName());
         person2.setName("Jane");
         System.out.println(person1.getName());
-        System.out.println(person2.getName());
-
+        System.out.println(person2.getName());*/
     }
-
-    public Person(String name) {
-        System.out.println(name);
-    }
-
-    private String name;
-
-    /*Create a `main` method on the class that creates a new `Person` object and
-    tests the above methods.*/
-        public String getName(){
-//TODO: return the person's name
-                return name;
-        }
-
-        public void setName(String name){
-//TODO: change the name property to the passed value
-            this.name = name;
-        }
-        public void sayHello(){
-//TODO: print a message to the console using the person's name
-            System.out.println(name);
-        }
-
-
-    }
+}
 
 
