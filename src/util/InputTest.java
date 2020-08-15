54d40eef22c64d46912806b6796fb9ab55263d9d
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-15 09:57:01 -0500

    tidying up the input test class

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,7 +1,15 @@
 package util;
 
+//These sout, call each method from the input class, for testing purposes.
+
 public class InputTest {
     public static void main(String[] args) {
-
+        Input scan = new Input();
+        System.out.println(scan.getString());
+        System.out.println(scan.yesNo());
+        System.out.println(scan.getInt(13,88));
+        System.out.println(scan.getInt());
+        System.out.println(scan.getDouble(1.56565, 88.9999));
+        System.out.println(scan.getDouble());
     }
 }
