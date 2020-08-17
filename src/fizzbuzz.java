c1f181ede7865200b218d508ca0308e1d347a691
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-17 09:23:54 -0500

    updating pushup

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -1,5 +1,18 @@
 public class fizzbuzz {
+    public static void fizzBuzz(int num) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
     public static void main(String[] args) {
-
+//        System.out.println(fizzBuzz(15))
     }
 }
