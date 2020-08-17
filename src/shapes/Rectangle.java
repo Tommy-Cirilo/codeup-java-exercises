c7be2eb917a64b1ce353ccb99ad963215646370e
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-17 16:27:32 -0500

    almost done with the polymorphism exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,9 +1,26 @@
 package shapes;
 
+/*Should have protected properties for both length and width. Rectangle
+should define a constructor that accepts two numbers for length and width,
+and sets those properties.
 
+Create two methods on the Rectangle class, getArea and getPerimeter that
+return the respective values.*/
 
 public class Rectangle {
-    public int length;
-    public int width;
+    protected int length;
+    protected int width;
 
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimeter() {
+        return 2 * length + 2 * width;
+    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,17 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle{
+    public Square(int side) {
+        super(side, side);
+    }
+
+    @Override
+    public int getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public int getPerimeter() {
+        return super.getPerimeter();
+    }
 }
