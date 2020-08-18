b37c866b013938252acf5a5d60589916cbf39eb6
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-17 23:47:35 -0500

    completed the polymorphism exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,7 +1,25 @@
 package shapes;
+/*create a variable of the type Rectangle named box1 and assign it a new instance of the
+Rectangle class with a width of 4 and a length of 5.
 
+verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+
+create a variable of the type Rectangle named box2 and assign it a new instance of the Square
+class that has a side value of 5.
+
+verify that the getPerimeter and getArea methods return 20 and 25, respectively.*/
 public class ShapesTest {
-    public static void main(String[] args) {
+    protected int box1;
+    protected int box2;
 
+    public static void main(String[] args) {
+        //creates a variable with Rectangle as type, and assigns it a new instance with parameters
+        Rectangle box1 = new Rectangle(4,5);
+        System.out.println("The perimeter of the rectangle is: " + box1.getPerimeter());
+        System.out.println("The area of the rectangle is: " + box1.getArea());
+        //creates a variable with square as type, and assigns it a new instance with parameters
+        Square box2 = new Square(4);
+        System.out.println("The perimeter of the square is: " + box2.getPerimeter());
+        System.out.println("The area of the square is: " + box2.getArea());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,15 +1,17 @@
 package shapes;
+/*extend Rectangle. Square should define a constructor that accepts one argument, side,
+and calls the parent's constructor to set both the length and width to the value of side.*/
 
 public class Square extends Rectangle{
     public Square(int side) {
         super(side, side);
     }
-
+    //getting the getArea method from the parent and returning it
     @Override
     public int getArea() {
         return super.getArea();
     }
-
+    //getting the getPerimeter method from the parent and returning it
     @Override
     public int getPerimeter() {
         return super.getPerimeter();
