e105ef264b1752f0d8788dc298392335952385c0
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-18 20:15:30 -0500

    working on interfaces and abstract exercises

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,5 +1,17 @@
 package shapes;
 
+
+/*Inside of shapes, create an abstract Quadrilateral class that extends Shape and implements Measurable.
+This class should have:
+
+protected properties for length and width.
+
+a constructor that accepts two numbers for the length and width and sets those properties.
+
+methods for getting the length and width.
+
+abstract methods for setting the length and width.*/
+
 public abstract class Quadrilateral extends Shape implements Measurable{
     protected double length;
     protected double width;
