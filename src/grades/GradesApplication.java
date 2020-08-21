98c2bc73421903c6318662285cbf29a5f1f9fcf8
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-20 23:55:55 -0500

    still working through the collection exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,7 +1,29 @@
 package grades;
 
+/*Create a map for students and GitHub usernames
+
+Create a class named GradesApplication with a main method. Inside the main method,
+ create a HashMap named students. It should have keys that are strings that
+ represent github usernames, and values that are Student objects. Create at least
+ 4 student objects with at least 3 grades each, and add them to the map.
+
+Be creative! Make up GitHub usernames and grades for your student objects.*/
+
+
+
+/*Create the command line interface
+
+Print the list of GitHub usernames out to the console, and ask the user which
+student they would like to see more information about. The user should enter a
+GitHub username (i.e. one of the keys in your map). If the given input does not
+match up with a key in your map, tell the user that no users with that username
+were found. If the given username does exist, display information about that
+student, including their name and their grades.
+
+After the information is displayed, the application should ask the user if they want
+ to continue, and keep running so long as the answer is yes.*/
+
 
 public class GradesApplication {
-    private Student name;
-    private Student grades;
+
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,37 @@
 package grades;
+/*should have private properties for the student's name, and grades. The grades
+property should be an ArrayList of integers. The student class should have a
+constructor that sets name property, and initializes the grades property as an
+empty ArrayList.*/
+
+/*Test your Student class by adding a main method and creating Student objects.
+Verify that you can add grades to each object, and that your getGradeAverage
+method correctly returns the average of the student's grades.*/
 
 public class Student {
+    private Student name;
+    private Student grades;
+
+
+    public Student getGrades() {
+        return grades;
+    }
+    //set Student's name
+    public void setName(Student name) {
+        this.name = name;
+    }
+    //set Student's grades
+    public void setGrades(Student grades) {
+        this.grades = grades;
+    }
+
+
+    // returns the student's name
+    public String getName(Student) {
+        return String name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade);
+    // returns the average of the students grades
+    public double getGradeAverage();
 }
